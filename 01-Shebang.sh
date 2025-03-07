6a98f91e29f3dd7c229fabf67f28e87585a91e51
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-08 04:15:37 +0530

    color adding to the text

--- 01-Shebang.sh
+++ 01-Shebang.sh
@@ -5,3 +5,6 @@ echo "I am yash and learning Bash now"
 echo "I \n Welcome home \n and town\t please have a \t seat\n take rest \t Thank you!"
 
 echo  -e "I \n Welcome home \n and town\t please have a \t seat\n take rest \t Thank you!"
+
+
+esho -e "\e [32m I am prinitng green \e [0m"
