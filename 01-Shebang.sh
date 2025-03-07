bef1ec7fd4a0c74b3f9d0611161100811147b3de
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-08 04:09:20 +0530

    adding escape sequence

--- 01-Shebang.sh
+++ 01-Shebang.sh
@@ -3,3 +3,5 @@
 echo "I am yash and learning Bash now"
 
 echo "I \n Welcome home \n and town\t please have a \t seat\n take rest \t Thank you!"
+
+echo "I \n Welcome home \n and town\t please have a \t seat\n take rest \t Thank you!"
