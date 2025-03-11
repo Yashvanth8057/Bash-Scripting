96f6ddca214ccf8b788d9a70c029b4ba68938681
YashavanthaN8057 <yme1061920@gmail.com>
2025-03-11 22:57:22 +0530

    doing exercise on loops

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -14,6 +14,27 @@ else echo "pr code is failed"
 fi
 
 
+echo "checking the services"
+
+
+ACTION=$1
+
+if [ "$ACTION" == "start" ]; then         # The below commands will only be exectuted if the condition is true.
+    echo -e "\e[32m Starting the service \e[0m"
+
+elif [ "$ACTION" == "stop" ]; then
+    echo -e "\e[31m Stopping the service \e[0m"
+
+elif [ "$ACTION" == "restart" ]; then
+    echo -e "\e[35m Restarting the service \e[0m"
+else
+    echo "Valid Arguments are start or stop or restart ONLY"
+    echo -e "Ex: \n\t \e[33m bash $0 start  \e[0m"
+fi 
+
+echo -e "Script Completed"
+
+
 
 
 
